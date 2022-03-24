e9fc3bc66f58af9021bd3fe3757dbda7ad5333fd
Swumplurd <53883345+Swumplurd@users.noreply.github.com>
2022-03-23 19:28:22 -0600

    imagenes normalizadas a 16:9

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -7,17 +7,28 @@ export const HomePage = () => {
     return (
         <>
             <div className="row">
-                <div className="col-6">
+                <div className="col-lg-6">
                     <Carousel images={[
-                        "/assets/erik_rauda_5.webp",
-                        "/assets/erik_rauda_7.webp",
-                        "/assets/erik_rauda_8.webp",
-                        "/assets/erik_rauda_9.webp",
-                        "/assets/erik_rauda_6.webp",
+                        "/assets/erik_rauda_1.jpg",
+                        "/assets/erik_rauda_2.jpg",
+                        "/assets/erik_rauda_3.jpg",
+                        "/assets/erik_rauda_4.jpg",
+                        "/assets/erik_rauda_5.jpg",
+                        "/assets/erik_rauda_6.jpg",
+                        "/assets/erik_rauda_7.jpg",
+                        "/assets/erik_rauda_8.jpg",
+                        "/assets/erik_rauda_9.jpg",
+                        "/assets/erik_rauda_10.jpg",
+                        "/assets/erik_rauda_11.jpg",
+                        "/assets/erik_rauda_12.jpg",
+                        "/assets/erik_rauda_13.jpg",
+                        "/assets/erik_rauda_14.jpg",
+                        "/assets/erik_rauda_15.jpg",
+                        "/assets/erik_rauda_16.jpg",
                     ]} id={'primer-carousel'}/>
                 </div>
-                <div className="col-6">
-                    <h3 className="display-6">
+                <div className="col-lg-6">
+                    <h3 className="display-6 text-center">
                         "La verdadera diferencia se genera
                         <Typewriter
                             options={{
@@ -32,6 +43,13 @@ export const HomePage = () => {
                             }}
                         />
                     </h3>
+                </div>
+            </div>
+            <div className="row my-4 justify-content-center">
+                <div className="col-lg-6">
+                    <div className="p-5 bg-light rounded-3 bg-secondary shadow">
+                        <Contacto/>
+                    </div>
                 </div>
             </div>
         </>
